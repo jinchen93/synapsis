3e2f039a9aeb059f8dec7cb3a23f7492fd6060f1
Jin Chen <jinchen93@gmail.com>
2017-06-28 12:49:43 -0700

    Add new production buld with webworker change

--- static/synapsis/bundle_neural_network.js
+++ static/synapsis/bundle_neural_network.js
@@ -2685,50 +2685,44 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 
 
 
-// Triggerred when another worker attempts to connect
-self.addEventListener("connect", function (e) {
-  // get port from connection
-  var port = e.ports[0];
+// get port from connection
+var port = self;
 
-  // create function callback for when steps occur
-  const post = (stats) => {
-    port.postMessage(stats);
-  };
+// create function callback for when steps occur
+const post = (stats) => {
+  port.postMessage(stats);
+};
 
-  // Fail console log method
-  const failCB = (e) => {
-    port.postMessage({error: e.stack});
-  };
+// Fail console log method
+const failCB = (e) => {
+  port.postMessage({error: e.stack});
+};
 
-  // Print console log method
-  const printCB = (e) => {
-    port.postMessage({type: "MESSAGE", e});
-  }
+// Print console log method
+const printCB = (e) => {
+  port.postMessage({type: "MESSAGE", e});
+}
 
-  // Debugger callbacks
-  self.logCB = printCB;
-  self.errorCB = failCB;
+// Debugger callbacks
+self.logCB = printCB;
+self.errorCB = failCB;
 
-  // listen in on the other thread for when messages are sent
-  port.addEventListener("message", function (e) {
-    try {
-      if (e.data.type === "INITIALIZE") {
-        // init network
-        const importUtil = new __WEBPACK_IMPORTED_MODULE_1__import_util__["a" /* default */](e.data.params);
-        this.network = new __WEBPACK_IMPORTED_MODULE_0__mnist_neural_network__["a" /* default */](post, importUtil, printCB, failCB);
-        this.network.run();
-      } else if (e.data.type === "RUN") {
-        this.network.run();
-      } else if (e.data.type === "PAUSE") {
-        this.network.pause();
-      }
-    } catch (e) {
-      port.postMessage({error: e.stack});
+// listen in on the other thread for when messages are sent
+port.addEventListener("message", function (e) {
+  try {
+    if (e.data.type === "INITIALIZE") {
+      // init network
+      const importUtil = new __WEBPACK_IMPORTED_MODULE_1__import_util__["a" /* default */](e.data.params);
+      this.network = new __WEBPACK_IMPORTED_MODULE_0__mnist_neural_network__["a" /* default */](post, importUtil, printCB, failCB);
+      this.network.run();
+    } else if (e.data.type === "RUN") {
+      this.network.run();
+    } else if (e.data.type === "PAUSE") {
+      this.network.pause();
     }
-  }, false);
-
-  // signal to the other thread that the connection has been made
-  port.start();
+  } catch (e) {
+    port.postMessage({error: e.stack});
+  }
 }, false);
 
 
